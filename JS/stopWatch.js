081942d641b39666b8f057dce4ee759d3a31c776
17Shanto <shahporanshanto23@gmail.com>
2023-03-14 20:33:52 +0600

    Done

--- JS/stopWatch.js
+++ JS/stopWatch.js
@@ -1,9 +1,45 @@
-// Set Random Color in Time Window
 
-document.getElementById('rand-color').addEventListener("click",function(){
-    setInterval(
-        function () {
-          var randomColor = Math.floor(Math.random()*16777215).toString(16);
-          document.body.style.backgroundColor = "#"+randomColor;
-        },1000);
-})
+const displayTime = document.getElementById('time');
+
+document.getElementById('start').addEventListener('click', function(){
+
+  const div = document.getElementById("color-field");
+
+  function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+  }
+  
+  function changeColor(){
+    div.style.backgroundColor= getRandomColor();
+  }
+
+  let timeTracker=0;
+
+const wannaStop = setInterval(()=>{
+    timeTracker++;
+    displayTime.innerText = '';
+    const insideCard = timeTracker;
+    displayTime.innerText = insideCard;
+    changeColor();
+    
+    document.getElementById('stop').addEventListener('click',function(){
+      clearInterval(wannaStop);
+    });
+
+    document.getElementById('reset').addEventListener('click',function(){
+      clearInterval(wannaStop);
+      displayTime.innerText = 0;
+
+    });
+
+
+},1000)
+  
+   //const id  = setInterval(changeColor,1000);
+
+});
